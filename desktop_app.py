80137796cc61752e3cc2398e9b4477cea980edfd
Emanuel Loborec <emanuel@infoplus.hr>
2020-12-01 20:56:43 +0100

    created layout, layout and logic connected

--- desktop_app.py
+++ desktop_app.py
@@ -2,55 +2,49 @@ import wx
 import matplotlib.pyplot as plt
 import os
 import sys
+import subprocess
 from PIL import Image
 
-directory = "/Users/emanuel/Downloads/Archive" # root - tu promijeni path u root folder gdje su ti slike
-
-class Mywin(wx.Frame): 
+#/Users/emanuel/Downloads/Archive
+class App(wx.Frame): 
     def __init__(self, parent, title): 
-      super(Mywin, self).__init__(parent, title = title,size = (200,150))  
-      panel = wx.Panel(self) 
-      vbox = wx.BoxSizer(wx.VERTICAL) 
+      super(App, self).__init__(parent, title = title,size = (400,150))  
+      panel = wx.Panel(self)
+      sizer = wx.GridBagSizer(4, 4)
       
-      #textbox
-      hbox1 = wx.BoxSizer(wx.HORIZONTAL) 
-      l1 = wx.StaticText(panel, -1, "Folder path") 
-		
-      hbox1.Add(l1, 1, wx.EXPAND|wx.ALIGN_LEFT|wx.ALL,5) 
-      self.t1 = wx.TextCtrl(panel) 
-		
-      hbox1.Add(self.t1,1,wx.EXPAND|wx.ALIGN_LEFT|wx.ALL,5) 
-      self.t1.Bind(wx.EVT_TEXT,self.OnKeyTyped) 
-      vbox.Add(hbox1) 
+      #description
+      text = wx.StaticText(panel, label="Folder path")
+      sizer.Add(text, pos=(0, 0), flag=wx.TOP|wx.LEFT|wx.BOTTOM, border=10)
+      
+      #input field
+      self.path = wx.TextCtrl(panel)
+      sizer.Add(self.path, pos=(1, 0), span=(1, 5), flag=wx.EXPAND|wx.LEFT|wx.RIGHT, border=10)
+      self.path.Bind(wx.EVT_TEXT,self.OnKeyTyped)
 
       #button
-      self.btn = wx.Button(panel,-1,"Remove Alpha channel") 
-      vbox.Add(self.btn,0,wx.ALIGN_CENTER) 
-      self.btn.Bind(wx.EVT_BUTTON,self.OnClicked)
-
+      button = wx.Button(panel, label="Remove Alpha", size=(140, 28))
+      self.Bind(wx.EVT_BUTTON, self.OnClicked, button)
+      sizer.Add(button, pos=(2, 4), flag=wx.RIGHT|wx.BOTTOM, border=10)
 
       #output
-      log = wx.TextCtrl(panel, size=(300,100))
-      sys.stdout=log
-
-  
-      hbox = wx.BoxSizer(wx.HORIZONTAL) 
-      vbox.Add(hbox,1,wx.ALIGN_CENTER) 
-      panel.SetSizer(vbox) 
-        
-      self.Centre() 
-      self.Show() 
-      self.Fit()
+      line = wx.TextCtrl(panel, wx.ID_ANY,style = wx.TE_MULTILINE|wx.TE_READONLY|wx.HSCROLL|wx.TE_RICH2)
+      sizer.Add(line, pos=(3, 0), span=(1, 5), flag=wx.EXPAND|wx.LEFT|wx.RIGHT, border=10)
+      sys.stdout = line
+      sizer.AddGrowableCol(1)
+      sizer.AddGrowableCol(3)
+      sizer.AddGrowableRow(3) 
+      panel.SetSizer(sizer)
 
     def OnKeyTyped(self, event):
-        directory=event.GetString()
+        self.path = event.GetString()
 
     def OnClicked(self, event):
-        btn = event.GetEventObject().GetLabel() 
-        print("Label of pressed button = ",btn )
-        program(directory)
+        button = event.GetEventObject().GetLabel()
+        #print("You pressed the button!")
+        program(self.path)
 
-def remove_alpha(image,):
+
+def remove_alpha(image):
     color=(255, 255, 255)
     image.load()  # treba za split()
     background = Image.new('RGB', image.size, color)
@@ -58,16 +52,20 @@ def remove_alpha(image,):
     return background
 
 def program(path):
-    for subdir, dirs, files in os.walk(directory):
+    for subdir, dirs, files in os.walk(path):
         for filename in files:
             if filename.endswith(".png"):
-                pic_path = directory+filename
+                pic_path = path+filename
                 pic = Image.open(os.path.join(subdir, filename))
                 if pic.mode in ('RGBA', 'LA') or (pic.mode == 'P' and 'transparency' in pic.info):
                     pic=remove_alpha(pic)
                     pic.save(os.path.join(subdir, filename))
-                    print(os.path.join(subdir, filename))  
+                    print(os.path.join(subdir, filename))
+                    proc = subprocess.Popen("ping %s", shell=True, stdout=subprocess.PIPE) 
+                    line = proc.stdout.readline()
+                      
 
 app = wx.App(redirect=True) 
-Mywin(None,  'PNG Alpha remover') 
+ex = App(None,  'PNG Alpha remover')
+ex.Show()
 app.MainLoop()
